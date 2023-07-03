346059b4e4c78fd97d51e17df25d4c2590570f12
AdityaAP7 <96766033+AdityaAP7@users.noreply.github.com>
2023-07-03 13:17:11 +0530

    Add files via upload

---
+++ src/assets/imgs.js
@@ -0,0 +1,84 @@
+export const imgs = [
+  {
+    name: "Elemental",
+    job: "Founder",
+    price: Math.floor(Math.random() * 100 + 1),
+    img: "https://i.seadn.io/gcs/files/1cfe1d2b3ceb546b8ec47672b58e3c0b.png?auto=format&dpr=1&w=1000",
+    rank: Math.floor(Math.random() * 980 + 1),
+  },
+  {
+    name: "MEGNUM",
+    job: "Co-Founder",
+    price: Math.floor(Math.random() * 100 + 1),
+
+    rank: Math.floor(Math.random() * 980 + 1),
+    img: "https://i.seadn.io/gcs/files/117a640b40722a28d773c8eb358128c4.png?auto=format&dpr=1&w=384",
+  },
+  {
+    name: "MONKEY KING",
+    job: "Director",
+    price: Math.floor(Math.random() * 100 + 1),
+    rank: Math.floor(Math.random() * 980 + 1),
+    img: "https://i.seadn.io/gcs/files/de28c23959d174eddd150b8a946f2e54.png?auto=format&dpr=1&w=384",
+  },
+  {
+    price: Math.floor(Math.random() * 100 + 1),
+    name: "BLACK PANTHER",
+    job: "Manager",
+    rank: Math.floor(Math.random() * 980 + 1),
+    img: "https://i.seadn.io/gcs/files/20e0bddc69fb71cebc3092c390a92f8b.png?auto=format&dpr=1&w=384",
+  },
+  {
+    price: Math.floor(Math.random() * 100 + 1),
+    name: "MONK",
+    job: "Web3 Developer",
+    rank: Math.floor(Math.random() * 980 + 1),
+    img: "https://i.seadn.io/gcs/files/c16b078fd154ae2194ad5bfa661c321a.png?auto=format&dpr=1&w=384",
+  },
+  {
+    price: Math.floor(Math.random() * 100 + 1),
+    name: "DEATHSTROKE",
+    job: "Artist",
+    rank: Math.floor(Math.random() * 980 + 1),
+    img: "https://i.seadn.io/gcs/files/d1581a5fd5636c39b68e7878cf891956.png?auto=format&dpr=1&w=384",
+  },
+  {
+    price: Math.floor(Math.random() * 100 + 1),
+    name: "LAZY KONG",
+    job: "Social Media Manager",
+    rank: Math.floor(Math.random() * 980 + 1),
+    img: "https://i.seadn.io/gcs/files/fa9f27fc7a1f7c7baf84cf776a9d0cc2.png?auto=format&dpr=1&w=384",
+  },
+  {
+    price: Math.floor(Math.random() * 100 + 1),
+    name: "CYBER PUNK",
+    job: "Blockchain Specialist",
+    rank: Math.floor(Math.random() * 980 + 1),
+    img: "https://i.seadn.io/gcs/files/8410ad510784a54283bdbaecdcc27088.png?auto=format&dpr=1&w=384",
+  },
+  {
+    price: Math.floor(Math.random() * 100 + 1),
+    name: "BANANA",
+    job: "Graphic Designer",
+    rank: Math.floor(Math.random() * 980 + 1),
+    img: "https://i.seadn.io/gcs/files/3fc7e200c864d1443e257193b6752f40.png?auto=format&dpr=1&w=384",
+  },
+  {
+    price: Math.floor(Math.random() * 100 + 1),
+
+    rank: Math.floor(Math.random() * 980 + 1),
+    img: "https://i.seadn.io/gcs/files/8edc2e9df6b2bc5d26abe51085581788.png?auto=format&dpr=1&w=384",
+  },
+  {
+    price: Math.floor(Math.random() * 100 + 1),
+
+    rank: Math.floor(Math.random() * 980 + 1),
+    img: "https://i.seadn.io/gcs/files/c2e325fc0d09d0f7d6d08d865e83dab5.png?auto=format&dpr=1&w=384",
+  },
+];
+
+export const arrow =
+  "  https://raw.githubusercontent.com/codebucks27/The-Weirdos-NFT-Website-Starter-Code/b8809acaea6698dae61762ed00494735ea1c2182/src/assets/Arrow.svg";
+
+export const e =
+  "https://raw.githubusercontent.com/codebucks27/The-Weirdos-NFT-Website-Starter-Code/b8809acaea6698dae61762ed00494735ea1c2182/src/assets/Ethereum.svg";
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,75 @@
+import React, { useRef, useState, createContext, useEffect } from "react";
+import Button from "../Button/Button";
+import "./NavBar.css";
+import NavBarMobile from "./NavBarMobile";
+
+// context api to sent the the state to the navBar mobile component
+export const isMobileNavOpenContext = createContext();
+const NavBar = () => {
+  //ref to the mobile menu icon
+  const mobileNavDivRef = useRef();
+
+  // state to set is the menu open or closed
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
+  const [firstRender, setFirstRender] = useState(false);
+  useEffect(() => {
+    if (isMobileNavOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    if (firstRender) {
+      mobileNavDivRef.current.classList.toggle("nav__mobile_icon_close");
+    } else {
+      setFirstRender(true);
+    }
+  }, [isMobileNavOpen,firstRender]);
+  const NavMobileClickHandler = () => {
+    setIsMobileNavOpen((pre) => !pre);
+  };
+  return (
+    <nav className="nav__nav section__padding flex__spaceBetween">
+      {isMobileNavOpen ? (
+        <isMobileNavOpenContext.Provider
+          value={[isMobileNavOpen, setIsMobileNavOpen, mobileNavDivRef]}
+        >
+          <NavBarMobile />
+        </isMobileNavOpenContext.Provider>
+      ) : (
+        <></>
+      )}
+      <div className="nav__logo_div">
+        <h1>A.</h1>
+      </div>
+      <div
+        ref={mobileNavDivRef}
+        onClick={NavMobileClickHandler}
+        className="nav__mobile_icon"
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className="nav__links_div">
+        <a href="#Home">Home</a>
+        <a href="#About">About</a>
+        <a href="#Roadmap">Roadmap</a>
+        <a href="#Showcase">Showcase</a>
+        <a href="#Team">Team</a>
+        <a href="#Faq">Faq</a>
+      </div>
+      {window.innerWidth <= 790 ? null : (
+        <Button
+          className="nav__button"
+          content="Connect Wallet"
+          borderColor="#FFC757"
+          bgc="FFC757"
+          color="white"
+        />
+      )}
+    </nav>
+  );
+};
+
+export default NavBar;
